8b5aafbda81d8cd954805338d0d14b0841bbfaf7
WnWn <144648184+hexo141@users.noreply.github.com>
2026-01-24 11:48:24 +0800

    使用python获取it权限

--- Mod/GetAdmin/GetAdmin.py
+++ Mod/GetAdmin/GetAdmin.py
@@ -6,6 +6,7 @@ import os
 import pathlib
 import subprocess
 import winreg
+import time
 import ctypes
 import sys
 from ctypes import wintypes
@@ -330,4 +331,362 @@ def GetSystem_by_Winlogon():
         import traceback
         traceback.print_exc()
         return None
+
+
+
+def Getit_by_Python():
+    print("Permissions may be incomplete")
+    
+
+    SC_MANAGER_CONNECT = 0x0001
+    SERVICE_START = 0x0010
+    SERVICE_STOP = 0x0020
+    SERVICE_QUERY_STATUS = 0x0004
+    SC_STATUS_PROCESS_INFO = 0
+    ERROR_SERVICE_NOT_ACTIVE = 1062
+    TOKEN_QUERY = 0x0008
+    TOKEN_DUPLICATE = 0x0002
+    TOKEN_ALL_ACCESS = 0xF01FF
+    SecurityImpersonation = 2
+    TokenPrimary = 1
+    PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
+    TH32CS_SNAPPROCESS = 0x00000002
+    INVALID_HANDLE_VALUE = -1
+    TokenUser = 1
+
+    # 定义 Windows 结构体
+    class SERVICE_STATUS(ctypes.Structure):
+        _fields_ = [
+            ('dwServiceType', wintypes.DWORD),
+            ('dwCurrentState', wintypes.DWORD),
+            ('dwControlsAccepted', wintypes.DWORD),
+            ('dwWin32ExitCode', wintypes.DWORD),
+            ('dwServiceSpecificExitCode', wintypes.DWORD),
+            ('dwCheckPoint', wintypes.DWORD),
+            ('dwWaitHint', wintypes.DWORD),
+        ]
+
+    class SERVICE_STATUS_PROCESS(ctypes.Structure):
+        _fields_ = [
+            ('dwServiceType', wintypes.DWORD),
+            ('dwCurrentState', wintypes.DWORD),
+            ('dwControlsAccepted', wintypes.DWORD),
+            ('dwWin32ExitCode', wintypes.DWORD),
+            ('dwServiceSpecificExitCode', wintypes.DWORD),
+            ('dwCheckPoint', wintypes.DWORD),
+            ('dwWaitHint', wintypes.DWORD),
+            ('dwProcessId', wintypes.DWORD),
+            ('dwServiceFlags', wintypes.DWORD),
+        ]
+
+    class PROCESSENTRY32W(ctypes.Structure):
+        _fields_ = [
+            ('dwSize', wintypes.DWORD),
+            ('cntUsage', wintypes.DWORD),
+            ('th32ProcessID', wintypes.DWORD),
+            ('th32DefaultHeapID', ctypes.POINTER(wintypes.ULONG)),
+            ('th32ModuleID', wintypes.DWORD),
+            ('cntThreads', wintypes.DWORD),
+            ('th32ParentProcessID', wintypes.DWORD),
+            ('pcPriClassBase', wintypes.LONG),
+            ('dwFlags', wintypes.DWORD),
+            ('szExeFile', wintypes.WCHAR * 260),
+        ]
+
+    class STARTUPINFOW(ctypes.Structure):
+        _fields_ = [
+            ('cb', wintypes.DWORD),
+            ('lpReserved', wintypes.LPWSTR),
+            ('lpDesktop', wintypes.LPWSTR),
+            ('lpTitle', wintypes.LPWSTR),
+            ('dwX', wintypes.DWORD),
+            ('dwY', wintypes.DWORD),
+            ('dwXSize', wintypes.DWORD),
+            ('dwYSize', wintypes.DWORD),
+            ('dwXCountChars', wintypes.DWORD),
+            ('dwYCountChars', wintypes.DWORD),
+            ('dwFillAttribute', wintypes.DWORD),
+            ('dwFlags', wintypes.DWORD),
+            ('wShowWindow', wintypes.WORD),
+            ('cbReserved2', wintypes.WORD),
+            ('lpReserved2', ctypes.POINTER(wintypes.BYTE)),
+            ('hStdInput', wintypes.HANDLE),
+            ('hStdOutput', wintypes.HANDLE),
+            ('hStdError', wintypes.HANDLE),
+        ]
+
+    class PROCESS_INFORMATION(ctypes.Structure):
+        _fields_ = [
+            ('hProcess', wintypes.HANDLE),
+            ('hThread', wintypes.HANDLE),
+            ('dwProcessId', wintypes.DWORD),
+            ('dwThreadId', wintypes.DWORD),
+        ]
+
+    class TOKEN_USER(ctypes.Structure):
+        _fields_ = [
+            ('SID', ctypes.c_void_p),
+            ('ATTRIBUTES', wintypes.DWORD),
+        ]
+
+    # 加载 Windows DLL
+    advapi32 = ctypes.windll.advapi32
+    kernel32 = ctypes.windll.kernel32
+
+    # 设置函数参数和返回类型
+    OpenSCManagerW = advapi32.OpenSCManagerW
+    OpenSCManagerW.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR, wintypes.DWORD]
+    OpenSCManagerW.restype = wintypes.SC_HANDLE
+
+    OpenServiceW = advapi32.OpenServiceW
+    OpenServiceW.argtypes = [wintypes.SC_HANDLE, wintypes.LPCWSTR, wintypes.DWORD]
+    OpenServiceW.restype = wintypes.SC_HANDLE
+
+    QueryServiceStatusEx = advapi32.QueryServiceStatusEx
+    QueryServiceStatusEx.argtypes = [wintypes.SC_HANDLE, ctypes.c_int, ctypes.c_void_p, wintypes.DWORD, ctypes.POINTER(wintypes.DWORD)]
+    QueryServiceStatusEx.restype = wintypes.BOOL
+
+    StartServiceW = advapi32.StartServiceW
+    StartServiceW.argtypes = [wintypes.SC_HANDLE, wintypes.DWORD, ctypes.POINTER(wintypes.LPWSTR)]
+    StartServiceW.restype = wintypes.BOOL
+
+    ControlService = advapi32.ControlService
+    ControlService.argtypes = [wintypes.SC_HANDLE, wintypes.DWORD, ctypes.POINTER(SERVICE_STATUS)]
+    ControlService.restype = wintypes.BOOL
+
+    QueryServiceStatus = advapi32.QueryServiceStatus
+    QueryServiceStatus.argtypes = [wintypes.SC_HANDLE, ctypes.POINTER(SERVICE_STATUS)]
+    QueryServiceStatus.restype = wintypes.BOOL
+
+    CloseServiceHandle = advapi32.CloseServiceHandle
+    CloseServiceHandle.argtypes = [wintypes.SC_HANDLE]
+    CloseServiceHandle.restype = wintypes.BOOL
+
+    OpenProcessToken = advapi32.OpenProcessToken
+    OpenProcessToken.argtypes = [wintypes.HANDLE, wintypes.DWORD, ctypes.POINTER(wintypes.HANDLE)]
+    OpenProcessToken.restype = wintypes.BOOL
+
+    GetTokenInformation = advapi32.GetTokenInformation
+    GetTokenInformation.argtypes = [wintypes.HANDLE, ctypes.c_int, ctypes.c_void_p, wintypes.DWORD, ctypes.POINTER(wintypes.DWORD)]
+    GetTokenInformation.restype = wintypes.BOOL
+
+    ConvertSidToStringSidW = advapi32.ConvertSidToStringSidW
+    ConvertSidToStringSidW.argtypes = [ctypes.c_void_p, ctypes.POINTER(wintypes.LPWSTR)]
+    ConvertSidToStringSidW.restype = wintypes.BOOL
+
+    LocalFree = kernel32.LocalFree
+    LocalFree.argtypes = [wintypes.HLOCAL]
+    LocalFree.restype = wintypes.HLOCAL
+
+    DuplicateTokenEx = advapi32.DuplicateTokenEx
+    DuplicateTokenEx.argtypes = [
+        wintypes.HANDLE, wintypes.DWORD, wintypes.LPVOID, 
+        ctypes.c_int, ctypes.c_int, ctypes.POINTER(wintypes.HANDLE)
+    ]
+    DuplicateTokenEx.restype = wintypes.BOOL
+
+    CreateProcessWithTokenW = advapi32.CreateProcessWithTokenW
+    CreateProcessWithTokenW.argtypes = [
+        wintypes.HANDLE, wintypes.DWORD, wintypes.LPCWSTR, wintypes.LPWSTR,
+        wintypes.DWORD, wintypes.LPVOID, wintypes.LPCWSTR, ctypes.POINTER(STARTUPINFOW),
+        ctypes.POINTER(PROCESS_INFORMATION)
+    ]
+    CreateProcessWithTokenW.restype = wintypes.BOOL
+
+    GetCurrentProcess = kernel32.GetCurrentProcess
+    GetCurrentProcess.restype = wintypes.HANDLE
+
+    CreateToolhelp32Snapshot = kernel32.CreateToolhelp32Snapshot
+    CreateToolhelp32Snapshot.argtypes = [wintypes.DWORD, wintypes.DWORD]
+    CreateToolhelp32Snapshot.restype = wintypes.HANDLE
+
+    Process32FirstW = kernel32.Process32FirstW
+    Process32FirstW.argtypes = [wintypes.HANDLE, ctypes.POINTER(PROCESSENTRY32W)]
+    Process32FirstW.restype = wintypes.BOOL
+
+    Process32NextW = kernel32.Process32NextW
+    Process32NextW.argtypes = [wintypes.HANDLE, ctypes.POINTER(PROCESSENTRY32W)]
+    Process32NextW.restype = wintypes.BOOL
+
+    GetModuleFileNameW = kernel32.GetModuleFileNameW
+    GetModuleFileNameW.argtypes = [wintypes.HMODULE, wintypes.LPWSTR, wintypes.DWORD]
+    GetModuleFileNameW.restype = wintypes.DWORD
+
+    OpenProcess = kernel32.OpenProcess
+    OpenProcess.argtypes = [wintypes.DWORD, wintypes.BOOL, wintypes.DWORD]
+    OpenProcess.restype = wintypes.HANDLE
+
+    CloseHandle = kernel32.CloseHandle
+    CloseHandle.argtypes = [wintypes.HANDLE]
+    CloseHandle.restype = wintypes.BOOL
+
+    GetLastError = kernel32.GetLastError
+    GetLastError.restype = wintypes.DWORD
+
+    Sleep = kernel32.Sleep
+    Sleep.argtypes = [wintypes.DWORD]
+    
+    # 内部辅助函数
+    def control_service(service_name, action):
+        hSCManager = OpenSCManagerW(None, None, SC_MANAGER_CONNECT)
+        if not hSCManager:
+            return False
+        
+        hService = OpenServiceW(hSCManager, service_name, SERVICE_START | SERVICE_STOP | SERVICE_QUERY_STATUS)
+        if not hService:
+            CloseServiceHandle(hSCManager)
+            return False
+        
+        isSuccess = False
+        
+        if action == "start":
+            ssStatus = SERVICE_STATUS_PROCESS()
+            dwBytesNeeded = wintypes.DWORD(0)
+            
+            if QueryServiceStatusEx(hService, SC_STATUS_PROCESS_INFO, ctypes.byref(ssStatus), 
+                                   ctypes.sizeof(SERVICE_STATUS_PROCESS), ctypes.byref(dwBytesNeeded)):
+                if ssStatus.dwCurrentState == 4:  # SERVICE_RUNNING
+                    isSuccess = True
+                else:
+                    if StartServiceW(hService, 0, None):
+                        for i in range(50):
+                            Sleep(30)
+                            if QueryServiceStatusEx(hService, SC_STATUS_PROCESS_INFO, ctypes.byref(ssStatus), 
+                                                   ctypes.sizeof(SERVICE_STATUS_PROCESS), ctypes.byref(dwBytesNeeded)) and ssStatus.dwCurrentState == 4:
+                                isSuccess = True
+                                break
+        elif action == "stop":
+            ssStatus = SERVICE_STATUS()
+            if ControlService(hService, 1, ctypes.byref(ssStatus)):  # SERVICE_CONTROL_STOP
+                for i in range(50):
+                    Sleep(100)
+                    if QueryServiceStatus(hService, ctypes.byref(ssStatus)) and ssStatus.dwCurrentState == 1:  # SERVICE_STOPPED
+                        isSuccess = True
+                        break
+            else:
+                if GetLastError() == ERROR_SERVICE_NOT_ACTIVE:
+                    isSuccess = True
+        
+        CloseServiceHandle(hService)
+        CloseServiceHandle(hSCManager)
+        return isSuccess
+
+    def get_system_pid():
+        pe = PROCESSENTRY32W()
+        pe.dwSize = ctypes.sizeof(PROCESSENTRY32W)
+        
+        hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+        if hSnap == INVALID_HANDLE_VALUE:
+            return 0
+        
+        pid = 0
+        if Process32FirstW(hSnap, ctypes.byref(pe)):
+            while True:
+                if pe.szExeFile.lower() == "services.exe":
+                    pid = pe.th32ProcessID
+                    break
+                if not Process32NextW(hSnap, ctypes.byref(pe)):
+                    break
+        
+        CloseHandle(hSnap)
+        return pid
+    
+    def get_ti_pid():
+        pe = PROCESSENTRY32W()
+        pe.dwSize = ctypes.sizeof(PROCESSENTRY32W)
+        
+        hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+        if hSnap == INVALID_HANDLE_VALUE:
+            return 0
+        
+        pid = 0
+        if Process32FirstW(hSnap, ctypes.byref(pe)):
+            while True:
+                if pe.szExeFile.lower() == "trustedinstaller.exe":
+                    pid = pe.th32ProcessID
+                    break
+                if not Process32NextW(hSnap, ctypes.byref(pe)):
+                    break
+        
+        CloseHandle(hSnap)
+        return pid
+    
+    def run_as_trusted_installer():
+        # 构建要运行的命令
+        target_exe = "cmd.exe"
+        parent_dir = pathlib.Path(__file__).parent.parent.parent
+        args = f'/c "cd /d "{parent_dir}" && "{sys.executable}" main.py"'
+        
+        print(f"Starting program with TrustedInstaller privileges: {target_exe} {args}\n(by Wnclient)")
+        
+        # 启动TrustedInstaller服务
+        if not control_service("TrustedInstaller", "start"):
+            print("Failed to start TrustedInstaller service")
+            return False
+        
+        time.sleep(1)
+        
+        # 获取TrustedInstaller进程PID
+        ti_pid = get_ti_pid()
+        if ti_pid == 0:
+            print("TrustedInstaller process not found")
+            control_service("TrustedInstaller", "stop")
+            return False
+        
+        # 打开TrustedInstaller进程
+        hProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, False, ti_pid)
+        if not hProc:
+            print(f"Failed to open process: {GetLastError()}")
+            control_service("TrustedInstaller", "stop")
+            return False
+        
+        # 获取进程令牌
+        hToken = wintypes.HANDLE()
+        if not OpenProcessToken(hProc, TOKEN_QUERY | TOKEN_DUPLICATE, ctypes.byref(hToken)):
+            print(f"Failed to open process token: {GetLastError()}")
+            CloseHandle(hProc)
+            control_service("TrustedInstaller", "stop")
+            return False
+        
+        # 复制令牌
+        hSysToken = wintypes.HANDLE()
+        if not DuplicateTokenEx(hToken, TOKEN_ALL_ACCESS, None, SecurityImpersonation, TokenPrimary, ctypes.byref(hSysToken)):
+            print(f"Failed to duplicate token: {GetLastError()}")
+            CloseHandle(hToken)
+            CloseHandle(hProc)
+            control_service("TrustedInstaller", "stop")
+            return False
+        
+        # 创建进程
+        si = STARTUPINFOW()
+        si.cb = ctypes.sizeof(STARTUPINFOW)
+        pi = PROCESS_INFORMATION()
+        
+        # 构建命令行
+        command_line = f'"{target_exe}" {args}'
+        
+        if not CreateProcessWithTokenW(
+            hSysToken, 0, target_exe, command_line, 0, None, None, 
+            ctypes.byref(si), ctypes.byref(pi)
+        ):
+            print(f"Startup failed, error code: {GetLastError()}")
+            success = False
+        else:
+            print("Startup successful!")
+            sys.exit(0)
+            CloseHandle(pi.hProcess)
+            CloseHandle(pi.hThread)
+            success = True
+        
+        # 清理资源
+        CloseHandle(hSysToken)
+        CloseHandle(hToken)
+        CloseHandle(hProc)
+        
+        # 停止TrustedInstaller服务
+        control_service("TrustedInstaller", "stop")
+        
+        return success
+    
+    return run_as_trusted_installer()
 
